38e7b02f5aa4dc6fed46e57540a1aeba79b1b1d7
tarang07 <iiittbharti7@gmail.com>
2016-07-06 20:55:35 +0530

    Add files via upload

---
+++ serv-master/requestdispatch/src/Q1/RequestDispatch.java
@@ -0,0 +1,58 @@
+package Q1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RequestDispatch
+ */
+@WebServlet("/RequestDispatch")
+public class RequestDispatch extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RequestDispatch() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");  
+	    PrintWriter out = response.getWriter();  
+	          
+	    String n=request.getParameter("userName");  
+	    String p=request.getParameter("userPass");  
+	          
+	    if(p.equals("servlet")){  
+	        RequestDispatcher rd=request.getRequestDispatcher("servlet2");  
+	        rd.forward(request, response);  
+	    }  
+	    else{  
+	        out.print("Username or/and Password Incorrect!");  
+	        RequestDispatcher rd=request.getRequestDispatcher("/index.html");  
+	        rd.include(request, response);  
+	                      
+	        }  
+	    }  
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
